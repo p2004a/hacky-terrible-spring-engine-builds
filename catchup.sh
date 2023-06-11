8d083681f14487b513b445df60afe6077c07b12b
Marek Rusinowski <marekrusinowski@gmail.com>
2023-06-11 21:30:49 +0200

    Add support for specifying from which commit to start syncing

--- catchup.sh
+++ catchup.sh
@@ -7,19 +7,28 @@ SCRIPT_DIR="$(dirname "$(realpath -s "$0")")"
 
 cd "$REPO"
 
-git checkout BAR105
-git submodule update --init --recursive
-git pull upstream BAR105
+SYNC_FROM=upstream/BAR105
+
+if [[ $# -ge 1 ]]; then
+    SYNC_FROM="$1"
+else
+    SYNC_FROM=upstream/BAR105
+    git checkout BAR105
+    git submodule update --init --recursive
+    git pull upstream BAR105
+fi
+
+git checkout "$SYNC_FROM"
 git submodule update --init --recursive
 
 distrobox enter spring -- "$SCRIPT_DIR/build.sh" windows last-catchup
 
-git checkout upstream/BAR105
+git checkout "$SYNC_FROM"
 git submodule update --init --recursive
 
 distrobox enter ubuntu18 -- "$SCRIPT_DIR/build.sh" linux last-catchup
 
-git checkout upstream/BAR105
+git checkout "$SYNC_FROM"
 git branch -f BAR105
 git checkout BAR105
 git submodule update --init --recursive
